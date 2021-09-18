a3312257e9cdcbcb49e03a4fa5b62f78fa7f8e8a
Brandon Gartner <brandonofthegartner@gmail.com>
2021-09-18 13:14:10 -0400

    have basic header and button component

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,7 +1,9 @@
-import {React} from 'react';
+import React, {Component} from 'react';
 
 class Page extends React.Component {
     render() {
-
+        return (<h1>test</h1>);
     }
 }
+
+export default Page;
