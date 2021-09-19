be85bb615463efaab19b15f5d0743dab1fe92726
George Shao <GeorgeShao123@gmail.com>
2021-09-19 02:05:55 -0400

    Changed class component to functional component

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Button from "../components/Button";
 import Rating from "../components/Rating";
 import Loading from "../components/Loading";
@@ -6,106 +6,54 @@ import background from "../media/ratingBackground.png";
 import LoadingScreen from "../components/LoadingScreen";
 import Cookies from "js-cookie";
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rating: 0,
-      recordedStarted: false,
-      displayButton: true,
-      displayLoad: false,
-      displayRating: false,
-      recordedFinished: false,  
-      irrelevantBoolean:
-    };
-    this.startRecording = this.startRecording.bind(this);
-  }
-
-  displayButton() {
-    this.setState({
-      displayRating: false,
-      displayButton: true,
-    })
-  }
-
-  displayLoad() {
-    this.setState({
-      displayButton: false,
-      displayLoad: true,
-    })
-  }
-
-  displayRating() {
-    this.setState({
-      displayLoad: false,
-      displayRating: true,
-    })
-  }
-
-  startRecording() {
-    this.setState({
-      recordedStarted: true,
-    });
+function Page() {
+  const [recordedStarted, setRecordedStarted] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const startRecording = () => {
+    setRecordedStarted(true);
     Cookies.set('five_seconds_done', 'f')
     Cookies.set('time_started_recording', Date.now())
   }
 
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          width: "100vw",
-          height: "100wh",
-          // backgroundImage: `url(${background})`,
-        }}
-      >
-        {Cookies.get("abc")}
-        {/* <Rating hertzValue={150} /> */}
-        <br />
-        {/* {this.state.recordedStarted ? (
-          <Loading />
-        ) : (
-          // <Rating hertzValue={150} />
-          <Button onClicked={this.startRecording} />
-        )} */}
-        {Cookies.get("five_seconds_done")}
-        {this.state.recordedStarted === false ? (
-          <Button onClicked={this.startRecording} />
-        ) : null}
-        {this.state.recordedStarted === true &&
-        Cookies.get("five_seconds_done") === "f" ? (
-          <Loading runOnComplete={this.displayRating}/>
-        ) : null}
-        {this.state.recordedStarted === true &&
-        Cookies.get("five_seconds_done") === "t" ? (
-          <Rating hertzValue={150} returnButton={this.displayButton}/>
-        ) : null}
-        {/* {this.state.displayButton ? 
-          <Button onClicked={this.startRecording} />
-          :
-          <div></div>
-        }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer(timer + 1);
+    }, 10000);
+    
+    return () => clearInterval(interval);
+  }, []);
 
-        {this.state.displayLoad ? (
-          <Loading runOnComplete={this.displayRating} />
-        ) :
-          <div></div>
-        }
-
-        {this.state.displayRating ? (
-          <Rating hertzValue={150} returnButton={this.displayButton}/>
-        ) :
-          <div></div>
-        } */}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        width: "100vw",
+        height: "100wh",
+      }}
+    >
+      {Cookies.get("abc")}
+      {Cookies.get("five_seconds_done")}
+      {recordedStarted === false ? (
+        <Button onClicked={startRecording}/>
+      ) : null}
+      {recordedStarted === true &&
+      Cookies.get("five_seconds_done") === "f" ? (
+        <Loading />
+      ) : null}
+      {recordedStarted === true &&
+      Cookies.get("five_seconds_done") === "t" ? (
+        <Rating hertzValue={150}/>
+      ) : null}
+    {/* </div>   */}
+  );
+}
         {/* {Cookies.get('five_seconds_done')}
         {this.state.recordedStarted === false ? (<Button recordedStarted={this.startRecording} />) : null}
         {this.state.recordedStarted === true && Cookies.get('five_seconds_done') == "f" ? (<Loading />) : null}
         {this.state.recordedStarted === true && Cookies.get('five_seconds_done') == "t" ? (<Rating />) : null} */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Page;
